2cf55864f7ad226b22217fe1df1bab37eeb094b9
rushikesharlekar <mca.1902@unigoa.ac.in>
2021-03-11 12:18:08 +0530

    UI card generation

--- ProjectUno/js/script.js
+++ ProjectUno/js/script.js
@@ -3,14 +3,23 @@ import Deck from "./deck.js"
 const deck = new Deck();
 deck.shuffle();
 
-// console.log(deck.shuffle());
+console.log(deck.shuffle());
 
 const player1 = document.getElementsByClassName("player1-deck")[0];
 const player2 = document.getElementsByClassName("player2-deck")[0];
 // console.log(player1);
 
-function getColor(color){
+function getColor(color, value){
+    
+    if(value == "+4"){
+        return "background-image: black;"; 
+    }
+    else if(value == "C"){
+        return "background-color: black;"; 
+    }
+    
     switch(color){
+
         case "R":
             return "background-color: red;";
             break;
@@ -26,16 +35,88 @@ function getColor(color){
     }
 }
 
+function getValueColor(value){
+    if(value == "+4" || value == "C"){
+        return "white"; 
+    }
+    else
+        return "black";
+}
+
+// function getRevCardImg(color){
+//     switch(color){
+
+//         case "R":
+//             return "url('../images/revRed.jpg')";
+//             break;
+//         case "B":
+//             return "url('../images/revBlue.png')";
+//             break;
+//         case "G":
+//             return "url('../images/revGreen.png')";
+//             break;
+//         case "Y":
+//             return "url('../images/revYellow.png')";
+//             break;
+//     }
+// }
+
+// function getSkipCardImg(color){
+//     switch(color){
+
+//         case "R":
+//             return "url('../images/revRed.jpg')";
+//             break;
+//         case "B":
+//             return "url('../images/revBlue.png')";
+//             break;
+//         case "G":
+//             return "url('../images/revGreen.png')";
+//             break;
+//         case "Y":
+//             return "url('../images/revYellow.png')";
+//             break;
+//     }
+// }
+
 function createCard(number,color){
     const card = document.createElement('div');
-    card.setAttribute("style", getColor(color));
+    card.setAttribute("style", getColor(color,number));
+    // card.style.backgroundColor = getColor(color, number);
+    card.style.color = getValueColor(number);
     card.className = "card";
 
     const topNumber = document.createElement('div');
     topNumber.innerText = number;
     topNumber.className = "top-number";
+
+    
     card.append(topNumber);
 
+    if(!(number == "+4" || number == "C" || number == "R" || number =="S")){
+        const cardNum = document.createElement('div');
+        cardNum.className = "card-number";
+        cardNum.innerText = number;
+        card.append(cardNum);
+        card.style.backgroundImage = "url('../images/"+color+".png')";
+        
+    }
+    else if(number == "+4"){
+        card.style.backgroundImage = "url('../images/fourCard.png')";
+    }
+    else if((number == "C")){
+        card.style.backgroundImage = "url('../images/colorCard.png')";
+        card.innerText = "";
+    }
+    else if(number == "R"){
+        card.style.backgroundImage = "url('../images/"+color+number+".png')";
+        card.innerText = "";
+    }
+    else {
+        card.style.backgroundImage = "url('../images/"+color+number+".png')";
+        card.innerText = "";
+    }
+    
     return card;
 }
 
